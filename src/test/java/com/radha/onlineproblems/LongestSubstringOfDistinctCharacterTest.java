6f997baf4f769df335574f60f30afaedcd359cf7
radhadevi11 <radhadevi16isr033@gmail.com>
2020-04-22 22:06:59 +0530

    LongestSubstringOfDistinctCharacter

---
+++ src/test/java/com/radha/onlineproblems/LongestSubstringOfDistinctCharacterTest.java
@@ -0,0 +1,67 @@
+package com.radha.onlineproblems;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LongestSubstringOfDistinctCharacterTest {
+
+
+    @Test
+    public void testGetSizeOfLongestSubString() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacter("abapqr")).isEqualTo(5);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringComplexExample() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacter("abababcdefababcdab")).isEqualTo(6);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringComplexExample2() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacter("geeksforgeeks")).isEqualTo(7);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringNonDuplicateCharacter() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacter("abcde")).isEqualTo(5);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringWithLessTimeComplexity() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacterWithLessTimeComplexity("abapqr")).isEqualTo(5);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringWithLessTimeComplexityComplexExample() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacterWithLessTimeComplexity("abababcdefababcdab")).isEqualTo(6);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringWithLessTimeComplexityComplexExample2() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacterWithLessTimeComplexity("geeksforgeeks")).isEqualTo(7);
+    }
+
+    @Test
+    public void testGetSizeOfLongestSubStringWithLessTimeComplexityNonDuplicateCharacter() {
+        LongestSubstringOfDistinctCharacter character = new LongestSubstringOfDistinctCharacter();
+
+        assertThat(character.getSizeOfLongestSubstringWithDistinctCharacter("abcde")).isEqualTo(5);
+    }
+
+
+}
