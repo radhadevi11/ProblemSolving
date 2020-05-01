0427d3bf44c442f07edff3f27d2d8c2c8c10c3c9
radhadevi11 <radhadevi16isr033@gmail.com>
2020-05-01 23:08:39 +0530

    BubbleSort implementation

---
+++ src/test/java/com/radha/onlineproblems/BubbleSortTest.java
@@ -0,0 +1,40 @@
+package com.radha.onlineproblems;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BubbleSortTest {
+
+    @Test
+    public void testSwap() {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] elements = {2, 1};
+
+        bubbleSort.swap(elements, 0, 1);
+
+        assertThat(elements).containsExactly(1, 2);
+
+
+    }
+
+    @Test
+    public void testSort() {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] elements = {2, 1, 5, 3, 6};
+
+        bubbleSort.sort(elements);
+
+        assertThat(elements).containsExactly(1, 2, 3, 5, 6);
+    }
+
+    @Test
+    public void printBubbleSortedArray() {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] elements = {2, 1, 5, 3, 6};
+
+        bubbleSort.printBubbleSortedArray(elements);
+
+        assertThat(elements).containsExactly(1, 2, 3, 5, 6);
+    }
+}
